c02941ee5e557d2ecc38ae9c134edd59234fb727
mahesh funde <maheshnfunde@yahoo.com>
2026-08-06 01:32:08 +0530

    added code for landing zone

---
+++ child_modules/subnet/main.tf
@@ -0,0 +1,9 @@
+#checkov:skip=CKV2_AZURE_31:NSG association will be implemented later
+
+resource "azurerm_subnet" "example" {
+  for_each             = var.subnet_name
+  name                 = each.value.name
+  resource_group_name  = each.value.resource_group_name
+  virtual_network_name = each.value.virtual_network_name
+  address_prefixes     = each.value.address_prefixes
+}
